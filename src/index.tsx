b64cd963f6194896a9b22c8132aa29f3be5e1bc1
Afzal Zubair <afzal@conzia.com>
2021-02-24 17:57:04 +0500

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import { Auth0Provider } from './contexts/auth0-context';
+import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<Auth0Provider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Auth0Provider>,
+	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
